e04cf42455a27b6272f5e8005a3ff7e91e900da0
ChristinaLear-coder <115825078+ChristinaLear-coder@users.noreply.github.com>
2023-02-18 21:51:16 +0200

    Update App.js
    bootstrap added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> React Weather App</h1>
-      <footer>
-        Coded by Christina Lear,{" "}
-        <a href="" target="_blank">
-          {" "}
-          open-sourced on GitHub and deployed by Netlify.
-        </a>
-      </footer>
+      <div className="container">
+        <h1> React Weather App</h1>
+        <footer>
+          Coded by Christina Lear,{" "}
+          <a href="" target="_blank">
+            {" "}
+            open-sourced on GitHub and deployed by Netlify.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
