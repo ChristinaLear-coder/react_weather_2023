33491ce5d12c5b5950d10b1c94ed7b7adab3e16d
ChristinaLear-coder <115825078+ChristinaLear-coder@users.noreply.github.com>
2023-02-18 22:24:01 +0200

    Update App.js
    Containers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1> React Weather App</h1>
+        <Weather />
         <footer>
           Coded by Christina Lear,{" "}
           <a href="" target="_blank">
